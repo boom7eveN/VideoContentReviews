7b0f1bf3100bfd7a8cf0b31b77ec602c96961cbc
Антон Телебоков <ForeverButNotAlone@yandex.ru>
2025-10-15 14:15:20 +0300

    Add repositories

---
+++ VideoContentReviews.DataAccess/Repositories/IBaseEntityRepository.cs
@@ -0,0 +1,12 @@
+﻿using VideoContentReviews.DataAccess.Entities;
+
+namespace VideoContentReviews.DataAccess.Repositories;
+
+public interface IBaseEntityRepository<T> where T : BaseEntity
+{
+    IQueryable<T> GetAll();
+    T? GetById(int id);
+    T? GetById(Guid id);
+    T Save(T entity);
+    void Delete(T entity);
+}
---
+++ VideoContentReviews.DataAccess/Repositories/IVideoContentGenreRepository.cs
@@ -0,0 +1,11 @@
+﻿using VideoContentReviews.DataAccess.Entities;
+
+namespace VideoContentReviews.DataAccess.Repositories;
+
+public interface IVideoContentGenreRepository
+{
+    IQueryable<VideoContentGenre> GetAll();
+    VideoContentGenre? GetById(Guid videoContentId, Guid genreId);
+    VideoContentGenre Save(VideoContentGenre entity);
+    void Delete(VideoContentGenre entity);
+}
